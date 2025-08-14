b982a4c60257adc9fc033f7dac922d95fdeebd67
Furkan Reçber <furkan@Furkan-MacBook-Pro.local>
2025-08-14 21:06:10 +0300

    Authentication add client

--- eCommerceClient/src/app/services/common/models/user.service.ts
+++ eCommerceClient/src/app/services/common/models/user.service.ts
@@ -3,12 +3,15 @@ import { HttpClientService } from '../http-client.service';
 import { User } from '../../../entities/user';
 import { Create_User } from '../../../contracts/users/create_user';
 import { firstValueFrom, Observable } from 'rxjs';
+import { Token } from '../../../contracts/token/token';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui/custom-toastr.service';
+import { TokenResponse } from '../../../contracts/token/tokenResponse';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  constructor(private httpClientService: HttpClientService) { }
+  constructor(private httpClientService: HttpClientService, private toastrService : CustomToastrService) { }
 
   async create(user: User): Promise<Create_User> {
     const observable: Observable<Create_User | User> = this.httpClientService.post<Create_User | User>({
@@ -17,4 +20,22 @@ export class UserService {
 
     return await firstValueFrom(observable) as Create_User;
   }
+
+  async login(usernameOrEmail: string, password: string, callbackFunction? : () => void) : Promise<any> {
+     const observable : Observable <any | TokenResponse> = this.httpClientService.post<any | TokenResponse>({
+      controller: "users",   
+      action: "login"
+    }, {usernameOrEmail, password} ) 
+
+    const tokenResponse  : TokenResponse =  await firstValueFrom(observable) as TokenResponse;  
+    if(tokenResponse) {
+      localStorage.setItem("accessToken", tokenResponse.token.accessToken);
+
+      this.toastrService.message("Login successful", "Success", {
+      messageType: ToastrMessageType.Success,
+      position: ToastrPosition  .TopRight
+      });
+    }    
+    callbackFunction(); // Call the callback function if provided
+  } 
 }
